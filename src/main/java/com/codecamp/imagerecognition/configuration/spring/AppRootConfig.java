e1bb84f5909d95df693fd8ff502ef9c2194a75c5
Slavcho Arsovski <slavcho.arsovski@netcetera.com>
2016-10-14 23:22:04 +0200

    web config

---
+++ src/main/java/com/codecamp/imagerecognition/configuration/spring/AppRootConfig.java
@@ -0,0 +1,10 @@
+package com.codecamp.imagerecognition.configuration.spring;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * App root spring configuration.
+ */
+@Configuration
+public class AppRootConfig {
+}
---
+++ src/main/java/com/codecamp/imagerecognition/web/WebComponents.java
@@ -0,0 +1,7 @@
+package com.codecamp.imagerecognition.web;
+
+/**
+ * Marker interface used for component scanning.
+ */
+public interface WebComponents {
+}
